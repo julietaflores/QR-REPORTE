8a6a722c7c17cd802f67e8b030afcf9534f9350a
Julieta Flores Tantani <jflores@monterrey.local>
2024-05-17 16:00:02 -0400

    version 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { LoginComponent } from './page/login/login.component';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
 import { InicioComponent } from './page/inicio/inicio.component';
 export const routes: Routes = [
-
       {
         path: '', title: 'QR REPORTE', component: LoginComponent,
       },
       {
         path: 'inicio',  title: 'QR REPORTE', component: InicioComponent,
       }
-
 ];
